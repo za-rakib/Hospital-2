64b617972ac67551aed36f00a530695d6ab81fe8
MD Rakib Hossen <phpzarakib@gmail.com>
2021-10-22 00:44:30 +0600

    first commit

---
+++ src/components/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../../Shared/Footer/Footer";
+import { Header } from "../Header/Header/Header";
+import Navbar from "../../Shared/Navbar/Navbar";
+
+export const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Header />
+      <Footer />
+    </div>
+  );
+};
